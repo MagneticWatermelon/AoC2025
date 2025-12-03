24b3e435c9a7b5ced13fd2914d6d64f11b46434a
Oguzhan Ipek <fgtyhtyh@hotmail.com>
2025-12-03 16:10:10 +0300

    Day 2 done

--- lib/day02.ex
+++ lib/day02.ex
@@ -6,20 +6,58 @@ defmodule AoC.Day02 do
   def part1(input) do
     input
     |> parse()
+    |> Enum.reduce(0, fn {first, last}, acc ->
+      for i <- first..last do
+        id = Integer.to_string(i)
+        mid = div(String.length(id), 2)
+        {first_halve, second_halve} = String.split_at(id, mid)
 
-    # TODO: implement solution
+        if first_halve == second_halve, do: i, else: 0
+      end
+      |> Enum.sum()
+      |> Kernel.+(acc)
+    end)
   end
 
   def part2(input) do
     input
     |> parse()
+    |> Enum.reduce(0, fn {first, last}, acc ->
+      for i <- first..last do
+        id = Integer.to_string(i)
+        divisors = get_divisors(String.length(id))
 
-    # TODO: implement solution
+        has_match =
+          Enum.any?(divisors, fn divisor ->
+            pattern_length = div(String.length(id), divisor)
+            pattern = String.slice(id, 0, pattern_length)
+            String.duplicate(pattern, divisor) == id
+          end)
+
+        if has_match, do: i, else: 0
+      end
+      |> Enum.sum()
+      |> Kernel.+(acc)
+    end)
   end
 
   defp parse(input) do
     input
     |> String.trim()
-    |> String.split("\n")
+    |> String.split(",")
+    |> Enum.map(&parse_range/1)
+  end
+
+  defp parse_range(range) do
+    String.split(range, "-", parts: 2)
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
+  end
+
+  defp get_divisors(id) do
+    for i <- 1..id,
+        rem(id, i) == 0,
+        i > 1,
+        do: i
   end
 end
--- test/day02_test.exs
+++ test/day02_test.exs
@@ -2,11 +2,12 @@ defmodule AoC.Day02Test do
   use ExUnit.Case
 
   @example_input """
+  11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
   """
 
   describe "part1/1" do
     test "solves example" do
-      # assert AoC.Day02.part1(@example_input) == :todo
+      assert AoC.Day02.part1(@example_input) == 1_227_775_554
     end
 
     test "solves puzzle" do
@@ -19,7 +20,7 @@ defmodule AoC.Day02Test do
 
   describe "part2/1" do
     test "solves example" do
-      # assert AoC.Day02.part2(@example_input) == :todo
+      assert AoC.Day02.part2(@example_input) == 4_174_379_265
     end
 
     test "solves puzzle" do
